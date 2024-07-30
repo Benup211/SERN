057ec7e1b02c4c6a8eecd941b1f0dccaffa53eb0
benup211 <benup211@gmail.com>
2024-07-30 22:19:38 +0545

    sequelize added with migration

---
+++ Day3/migrations/20240730014703-create-user.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password:{
+        type: Sequelize.STRING
+      },
+      address:{
+        type: Sequelize.STRING
+      },
+      phoneNo:{
+        type: Sequelize.STRING
+      },
+      desc:{
+        type: Sequelize.STRING,
+        defaultValue: 'No description available',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
